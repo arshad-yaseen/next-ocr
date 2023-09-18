1aa19d84c3524269b787dbcdf809555d61e84010
arshad-yaseen <arshadpyaseen@gmail.com>
2023-09-18 21:20:35 +0530

    chore: finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,81 @@
-import Image from 'next/image'
+"use client";
+
+import { createWorker } from "tesseract.js";
+import { pdfToImg } from "@/lib/pdf-to-img";
+import { useState } from "react";
 
 export default function Home() {
+  const [pdfContent, setPdfContent] = useState<string[]>([]);
+  const [status, setStatus] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleExtractPdf = async (file: File) => {
+    if (!file) return;
+    try {
+      const images = await pdfToImg(file);
+      const pages = [];
+
+      for (let i = 0; i < images.length; i++) {
+        const image = images[i];
+        const worker = await createWorker({
+          logger: (m) => setStatus(m.status),
+        });
+
+        await worker.load();
+        await worker.loadLanguage("eng");
+        await worker.initialize("eng");
+        const {
+          data: { text },
+        } = await worker.recognize(image);
+
+        // Pushing the extracted text from each page to the pages array
+        pages.push(text);
+
+        await worker.terminate();
+      }
+
+      return pages;
+    } catch (error) {
+      console.error("Error extracting PDF:", error);
+    }
+  };
+
+  const handleFileUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const files = event.target.files;
+    if (!files) return;
+    const file = files[0];
+    setLoading(true);
+    const pdfContent = await handleExtractPdf(file);
+    setLoading(false);
+    setPdfContent(pdfContent!);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center sm:p-24 p-4">
+      <input type="file" accept=".pdf" onChange={handleFileUpload} />
+
+      {
+        loading && status && (
+          <p className="mt-28">
+            {status}
+          </p>
+        )
+      }
+
+      {pdfContent &&
+        pdfContent.map((page, index) => (
+          <div
+            key={index}
+            className="sm:w-[800px] w-full mt-6 bg-background border border-input rounded-md p-6"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <p className="text-lg font-medium mt-4">
+              Page {index + 1}
+            </p>
+            <p className="mt-4">{page}</p>
+          </div>
+        ))}
     </main>
-  )
+  );
 }
---
+++ lib/pdf-to-img.ts
@@ -0,0 +1,64 @@
+// @ts-ignore
+import * as pdfjsLib from "pdfjs-dist/build/pdf";
+import { PDFPageProxy } from "pdfjs-dist/types/src/display/api";
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+
+const loadPdf = async (file: File): Promise<PDFPageProxy[]> => {
+  const uri = URL.createObjectURL(file);
+  const pdf = await pdfjsLib.getDocument({ url: uri }).promise;
+
+  const pages: pdfjsLib.PDFPageProxy[] = [];
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i);
+    pages.push(page);
+  }
+  return pages;
+};
+
+// pdf-to-img.ts
+const renderPageToImage = async (page: pdfjsLib.PDFPageProxy, scale: number = 3): Promise<string> => {
+    const viewport = page.getViewport({ scale });
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+  
+    if (!canvas || !context) {
+      throw new Error("Canvas or context is null.");
+    }
+  
+    const pixelRatio = window.devicePixelRatio || 1;
+    canvas.width = viewport.width * pixelRatio;
+    canvas.height = viewport.height * pixelRatio;
+    context.scale(pixelRatio, pixelRatio);
+  
+    context.imageSmoothingEnabled = true;
+    context.imageSmoothingQuality = "high";
+  
+    const renderContext = {
+      canvasContext: context,
+      viewport: viewport,
+      enableWebGL: false,
+    };
+  
+    const renderTask = page.render(renderContext);
+  
+    await renderTask.promise;
+  
+    return canvas.toDataURL();
+  };
+
+export const pdfToImg = async (file: File): Promise<string[]> => {
+  try {
+    const pages = await loadPdf(file);
+    const images: string[] = [];
+
+    for (const page of pages) {
+      const image = await renderPageToImage(page);
+      images.push(image);
+    }
+
+    return images;
+  } catch (error) {
+    console.error("PDF error:", error);
+    return [];
+  }
+};
